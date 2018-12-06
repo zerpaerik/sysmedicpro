3d86d7cce4b2d45356ebb7f2f01d2a481814e6b4
Erik Zerpa <zerpaerik@gmail.com>
2018-12-06 15:14:45 -0400

    subiendo proyecto

---
+++ resources/views/archivos/servicios/add_items.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts.app')
+
+@section('content')
+<br>
+<div class="row">
+	<div class="col-xs-12">
+		<div class="box">
+			<div class="box-header">
+				<div class="box-name">
+					<i class="fa fa-users"></i>
+					<span><strong>Agregar Servicio</strong></span>
+				</div>
+				<div class="box-icons">
+					<a class="collapse-link">
+						<i class="fa fa-chevron-up"></i>
+					</a>
+					<a class="expand-link">
+						<i class="fa fa-expand"></i>
+					</a>
+				</div>
+				<div class="no-move"></div>
+			</div>
+			<div class="box-content">
+				<h4 class="page-header"></h4>
+				<form class="form-horizontal" role="form" method="post" action="servicios/storeItems/{{$servicio->id}}">
+					{{ csrf_field() }}
+					<div class="form-group">
+						<label class="col-sm-1 control-label">Detalle</label>
+						<div class="col-sm-3">
+							<input type="text" class="form-control" name="detalle" placeholder="Detalle" data-toggle="tooltip" data-placement="bottom" title="Detalle" value="{{$servicio->detalle}}">
+						</div>
+						<label class="col-sm-1 control-label">Precio</label>
+						<div class="col-sm-3">
+							<input type="text" class="form-control" name="precio" placeholder="Precio" data-toggle="tooltip" data-placement="bottom" title="Precio" value="{{$servicio->precio}}">
+						</div>
+
+						<label class="col-sm-1 control-label">Porcentaje</label>
+						<div class="col-sm-3">
+							<input type="text" class="form-control" name="porcentaje" placeholder="porcentaje" data-toggle="tooltip" data-placement="bottom" title="porcentaje" value="{{$servicio->porcentaje}}">
+						</div>
+						
+								
+
+						<br>
+						<input type="submit" style="margin-left:15px; margin-top: 20px;" class="col-sm-2 btn btn-primary" value="Agregar">
+
+						<a href="{{route('servicios.index')}}" style="margin-left:15px; margin-top: 20px;" class="col-sm-2 btn btn-danger">Volver</a>
+					</div>			
+				</form>	
+			</div>
+		</div>
+	</div>
+</div>
+@section('scripts')
+<script src="{{ asset('plugins/sheepit/jquery.sheepItPlugin.min.js') }}" type="text/javascript"></script>
+
+<script type="text/javascript">
+  $(document).ready(function() {
+	    // Main sheepIt form
+	    var phonesForm = $("#materiales").sheepIt({
+	        separator: '',
+	        allowRemoveCurrent: true,
+	        allowAdd: true,
+	        allowRemoveAll: true,
+	        allowRemoveLast: true,
+
+	        // Limits
+	        maxFormsCount: 10,
+	        minFormsCount: 0,
+	        iniFormsCount: 0,
+	        removeAllConfirmationMsg: 'Seguro que quieres eliminar todos?'
+	    });   
+	});
+
+</script>
+@endsection
+@endsection
---
+++ resources/views/archivos/servicios/create.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.app')
+
+@section('content')
+<br>
+<div class="row">
+	<div class="col-xs-12">
+		<div class="box">
+			<div class="box-header">
+				<div class="box-name">
+					<i class="fa fa-users"></i>
+					<span><strong>Agregarrrr Servicio</strong></span>
+				</div>
+				<div class="box-icons">
+					<a class="collapse-link">
+						<i class="fa fa-chevron-up"></i>
+					</a>
+					<a class="expand-link">
+						<i class="fa fa-expand"></i>
+					</a>
+				</div>
+				<div class="no-move"></div>
+			</div>
+			<div class="box-content">
+				<h4 class="page-header"></h4>
+				<form class="form-horizontal" role="form" method="post" action="servicios/create">
+					{{ csrf_field() }}
+					<div class="form-group">
+						<label class="col-sm-2 control-label">Detalle</label>
+						<div class="col-sm-4">
+							<input type="text" class="form-control" name="detalle" placeholder="Detalle" data-toggle="tooltip" data-placement="bottom" title="Detalle">
+						</div>
+						<label class="col-sm-2 control-label">Precio</label>
+						<div class="col-sm-4">
+							<input type="text" class="form-control" name="precio" placeholder="Precio" data-toggle="tooltip" data-placement="bottom" title="Precio">
+						</div>
+
+						<label class="col-sm-2 control-label">Porcentaje</label>
+						<div class="col-sm-4">
+							<input type="text" class="form-control" name="porcentaje" placeholder="porcentaje" data-toggle="tooltip" data-placement="bottom" title="porcentaje">
+						</div>
+						
+						</div>
+
+								
+						<br>
+						<input type="submit" style="margin-left:15px; margin-top: 20px;" class="col-sm-2 btn btn-primary" value="Agregar">
+
+						<a href="{{route('servicios.index')}}" style="margin-left:15px; margin-top: 20px;" class="col-sm-2 btn btn-danger">Volver</a>
+					</div>			
+				</form>	
+			</div>
+		</div>
+	</div>
+</div>
+@section('scripts')
+
+
+@endsection
+@endsection
